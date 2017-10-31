46fedc190c14e9cf7bd32363f9d4c0949e2d887e
Takashi Ishio <ishio@is.naist.jp>
2017-10-31 10:20:12 +0900

    Optimized a directory traversal.

--- src/sarf/lexer/DirectoryScan.java
+++ src/sarf/lexer/DirectoryScan.java
@@ -1,12 +1,11 @@
 package sarf.lexer;
 
 import java.io.File;
-
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 
 /**
- * This main class simply executes a lexer and output a list of tokens. 
+ * A class to traverse files in a directory. 
  * @author ishio
  *
  */
@@ -15,20 +14,26 @@ public class DirectoryScan {
 	public interface Action {
 		public void process(File f);
 	}
-	
 
+	/**
+	 * Search all files in a directory recursively.
+	 * @param dir specifies a search root.
+	 * @param action is called for each file.
+	 */
 	public static void scan(File dir, Action action) {
-		LinkedList<File> files = new LinkedList<File>();
+		ArrayList<File> files = new ArrayList<File>();
 		files.add(dir);
 		while (!files.isEmpty()) {
-			File f = files.removeFirst();
+			File f = files.remove(files.size()-1);
 			if (f.isDirectory() && f.canRead()) {
 				File[] children = f.listFiles();
 				for (File c: children) {
-					if ((c.isDirectory() &&
+					if (c.isDirectory() &&
 						!c.getName().equals(".") && 
-					    !c.getName().equals("..")) || c.isFile()) {
-						files.addFirst(c);
+						!c.getName().equals("..")) {
+						files.add(c);
+					} else if (c.isFile() && f.canRead()) {
+						action.process(f);
 					}
 				}
 			} else if (f.isFile() && f.canRead()) {
